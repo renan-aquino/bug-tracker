d28bed480a7a82d700b49881b5276124f83ad48e
Renan Aquino <renan.aquino99@gmail.com>
2023-09-07 16:55:43 -0300

    refactor: moved components to src directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import { Navbar } from './components/navbar/navbar'
+import { Navbar } from '@/components/navbar/navbar'
+import { TicketGrid } from '@/components/ticket-grid/ticket-grid'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar/>
+      <TicketGrid/>
     </main>
   )
 }
