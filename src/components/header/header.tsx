9cc7b68f66cf9bcc385da15b9e0e22bdb80eadf7
Renan Aquino <renan.aquino99@gmail.com>
2023-09-14 02:28:55 -0300

    feat: logo redirects to home

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,19 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
 import s from './header.module.css'
 
 export default function Header(){
+    const router = useRouter()
+
+    const handleClick = () => {
+        router.push('/tickets')
+    }
+    
     return (
         <header className={s.header_tag}>
             <div className={s.container}>
-                <a className={s.logo}><span>Bug</span> Tracker</a>
+                <a onClick={handleClick} className={s.logo}><span>Bug</span> Tracker</a>
                 
                 <div className={s.login}>
                     <p>Username</p>
