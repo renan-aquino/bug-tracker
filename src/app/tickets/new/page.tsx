58d1afd9652169cc91fa76bb61847adbae8b63b4
Renan Aquino <renan.aquino99@gmail.com>
2023-10-24 23:56:57 -0300

    feat: improved usability for ticket form

--- src/app/tickets/new/page.tsx
+++ src/app/tickets/new/page.tsx
@@ -1,8 +1,12 @@
+import { BackButton } from '@/components/back-button/back-button';
+import s from './page.module.css'
 import { TicketForm } from "@/components/ticket-form/ticket-form";
 
 export default function New(){
     return (
-        <main>
+        <main className={s.container}>
+            <div className={s.back_button}><BackButton/></div>
+            <h2>New Ticket</h2>
             <TicketForm/>
         </main>
     )
--- src/components/ticket-form/ticket-form.tsx
+++ src/components/ticket-form/ticket-form.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import s from './ticket-form.module.css'
 import { TicketRequestDTO, TicketResponseDTO, useTicketMutate } from "@/hooks/useTicketMutate"
 import { useRouter } from "next/navigation"
 import { useState } from 'react'
@@ -22,9 +23,11 @@ export function TicketForm(){
     }
 
     return (
-        <form action="" onSubmit={(e) => {e.preventDefault()}}>
-            <input type="text" onChange={e => setTitle(e.target.value)} />
-            <textarea onChange={e => setText(e.target.value)} cols={30} rows={10}></textarea>
+        <form className={s.container} action="" onSubmit={(e) => {e.preventDefault()}}>
+            <h3>Title</h3>
+            <input id='title' type="text" onChange={e => setTitle(e.target.value)} />
+            <h3>Message</h3>
+            <textarea id='message' onChange={e => setText(e.target.value)} cols={30} rows={10}></textarea>
             <button onClick={submit}>Publish</button>
         </form>
     )
